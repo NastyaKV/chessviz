e81aac5b989246e47a7cbbd0423ceb79636974ee
KnastyaAV <cydoka@mail.ru>
2018-02-28 22:15:39 +0700

    Create chess table

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf(" _a_b_c_d_e_f_g_h\n1|f h e w k e h f|\n2|p p p p p p p p|\n3|               |\n4|               |\n5|               |\n6|               |\n7|p p p p p p p p|\n8|f h e k w e h f|");
+	return 0;
+}
